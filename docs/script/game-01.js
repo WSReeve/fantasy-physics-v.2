b050b88747acf74beba7526c70d1b1bfb87875cc
WSReeve <wreeve4@gmail.com>
2020-08-20 10:41:18 -0500

    asdfghjkl;

--- docs/script/game-01.js
+++ docs/script/game-01.js
@@ -1,26 +1,39 @@
-let deck;
-let card;
-let cardDir;
-let objective;
+const opponentArea = document.querySelector('.opponent-card');
+const opponentDeck = ['Particle','Device','Concept'];
 
-const Deck = function(deckTitle, deckDesc, deckId) {
-    this.deckTitle = deckTitle;
-    this.deckDesc = deckDesc;
-    this.deckId = deckId;
+let opponentCardType;
+
+let playerCardContent = playerCard.children[1];
+let playerCardContentType = playerCardContent.children[1];
+
+let playerCardType = playerCardContentType.innerHTML;
+
+function chooseRndmCard() {
+    let max = opponentDeck.length;
+    let cardIndex = Math.floor(Math.random() * Math.floor(max));
+    opponentCardType = opponentDeck[cardIndex];
 }
 
-const Objective = function(objName, objType) {
-    this.objName = objName;
-    this.objType = objType;
-}
-
-const Card = function(cardName, cardType, cardText, cardId) {
-    this.cardName = cardName;
-    this.cardType = cardType;
-    this.cardText = cardText;
-    this.cardId = cardId;
-}
-
-const CardDirectory = function(cardIdList) {
-    this.cardIdList = cardIdList;
+function compareCards() {
+    if (playerCardType === opponentCardType) {
+        alert('DRAW');
+    } else if (playerCardType === "Particle") {
+        if (opponentCardType === "Concept") {
+            alert('YOU WIN!');  
+        } else {
+            alert('YOU LOOSE!');
+        }
+    } else if (playerCardType === "Device") {
+        if (opponentCardType === "Particle") {
+            alert('YOU WIN!');  
+        } else {
+            alert('YOU LOOSE!');
+        }
+    } else if (playerCardType == "Concept") {
+        if (opponentCardType === "Device") {
+            alert('YOU WIN!');  
+        } else {
+            alert('YOU LOOSE!');
+        }
+    } 
 }
--- docs/script/main-01.js
+++ docs/script/main-01.js
@@ -1,42 +1,69 @@
 
+const clock = document.querySelector(".timer");
+const initTime = 10;
+let time = initTime;
+let runTimer
 
+function countDown() {
+    clock.innerHTML = `${time}`;
 
-
-
-let DeckSelect = {
-    setGameStart: function(deckId) {
-        this.resetDeck(deckId);
-        this.setStage();
-        this.getHand();
-    },
-    resetDeck: function(deckId) {
-        switch (deckId) {
-            case "d-01":
-                deck = new Deck(document.querySelector('.deck--1.title').textContent, document.querySelector('.deck--1.desc').textContent, deckId);
-                cardDir = new CardDirectory ([]);
-                break;
-            case "d-02":
-                deck = new Deck(document.querySelector('.deck--2.title').textContent, document.querySelector('.deck--2.desc').textContent, deckId);
-                break;
-            case "d-03":
-                deck = new Deck(document.querySelector('.deck--3.title').textContent, document.querySelector('.deck--3.desc').textContent, deckId);
-                break;
-            case "d-04":
-                deck = new Deck(document.querySelector('.deck--4.title').textContent, document.querySelector('.deck--4.desc').textContent, deckId);
-                break;
-            case "d-05":
-                deck = new Deck(document.querySelector('.deck--5.title').textContent, document.querySelector('.deck--5.desc').textContent, deckId);
-                break;
-            default:
-                console.log('error');
-                break;
-        }
-    },
-    setStage: function() {
-        let getInterface = document.querySelector('.interface');
-        getInterface.innerHTML = "<main><h1>You Chose " + deck.deckTitle + "</h1> <div class='cards'><div class='card__wrapper'> <div class='card card--1'><img src='card-imgs/card-proton.png' alt='Proton Particle'><div class='card__content'><h2 class='card__name'>Proton</h2><h3 class='card__type'>Particle</h3><div class='card__text'><p>desc.</p></div></div></div> <div class='card card--2'><img src='card-imgs/card-muon.png' alt='Muon Particle'><div class='card__content'><h2 class='card__name'></h2><h3 class='card__type'></h3><div class='card__text'><p></p></div></div></div> <div class='card card--3'><img src='card-imgs/card-higgs.png' alt='Higgs Particle'><div class='card__content'><h2 class='card__name'></h2><h3 class='card__type'></h3><div class='card__text'><p></p></div></div></div> </div></div></main>";
-    },
-    getHand: function() {
-
+    if (time < 1) {
+        clock.innerHTML = `play`;
+        time = initTime;
+        clearInterval(runTimer);
+        compareCards();
     }
+
+    time--;
+}
+
+clock.addEventListener("click", (event) => {
+    chooseRndmCard();
+    runTimer = setInterval(countDown,1000);
+});
+
+
+const cards = document.querySelectorAll(".card");
+const areas = document.querySelectorAll(".area");
+let playerCard;
+
+
+for(const card of cards) {
+    card.addEventListener('dragstart', dragStart);
+    card.addEventListener('dragend', dragEnd);
+}
+
+for(const area of areas) {
+    area.addEventListener('dragover', dragOver);
+    area.addEventListener('dragenter', dragEnter);
+    area.addEventListener('dragleave', dragLeave);
+    area.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+    playerCard = this;
+    this.classList.add('clicked');
+    setTimeout(() => this.classList.add('invisible'), 0);
+    console.log(`start ${this.className}`);
+}
+
+function dragEnd() {
+    this.classList.remove('clicked');
+    this.classList.remove('invisible');
+    console.log(`end ${playerCard}`);
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+}
+
+function dragDrop() {
+    this.appendChild(playerCard);
 }
